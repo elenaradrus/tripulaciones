bc1e5ca47c4da690ec79e45e4b0a2177178a43fa
Anibal Montero <anibal.montero.p@gmail.com>
2022-07-23 12:39:12 +0200

    scaffold backend

---
+++ server/routes/users.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/users.controller');
+
+router.get('/', getUsers);
+router.get('/:id', getUser);
+router.post(
+  '/',
+  [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    // check('password', 'Password entre 6 y 12 caracteres').isLength({
+    //   min: 6,
+    //   max: 20,
+    // }),
+  ],
+  createUser
+);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+module.exports = router;
