0cf5291d27e63ab37147ad016510a9eb8ca521c6
masugerencias <yannpoirotsanz@gmail.com>
2022-07-31 22:08:38 +0200

    mirama en heroku

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,8 +3,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import { signInWithGoogle } from '../helpers/firebase';
 import { useForm } from '../hooks';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
 
 import './LoginForm.css';
 export const LoginForm = () => {
