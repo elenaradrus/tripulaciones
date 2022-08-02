3f5efccf573175abed7b3411b2197b11c6b69482
Elena Radu <lena@MacBook-Air-de-Elena.local>
2022-08-02 17:22:24 +0200

    pagina progreso

--- client/src/components/pages/Progreso.js
+++ client/src/components/pages/Progreso.js
@@ -1,10 +1,31 @@
+import './Progreso.css'
+
 export const Progreso = () => {
-    return ( 
-        <div>
+
+    return (
+        <div className='containerProgreso'>
             <div className='weekCompleted'>
-                <img src='images/semana.png'/>
+                <div>
+                    <img className='imagenSemana' src='/images/semana.png' alt="semana" />
+                </div>
             </div>
+            <div className='textDiaConsecutivo'>
+                <h3>¡6to día <br />consecutivo!</h3>
+            </div>
+
+            <div className='containerimagenSemana'>
+                <img className='imagenSilueta' src='/images/completadoSemana.png' alt="semana" />
+            </div>
+
+            <h3 className='desbloqueado'>Has desbloqueado el Nivel 2 <br />en Navega Seguro</h3>
+
+            <div className='containerBtn'>
+                <button className='btnProgreso'>Sigue <br/>Aprendiendo</button>
+            </div>
+
+
+
         </div>
     );
 }
- 
+
--- client/src/components/pages/RespuestasPage.js
+++ client/src/components/pages/RespuestasPage.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './RespuestasPage.css';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 
 function RespuestasPage() {
+
+  const navigate = useNavigate();
+
+  const weeklyOnClick = () => {
+    navigate('/progreso')
+  }
+
   return (
     <div>
         <h2 className='aprendamosTitle'>Completado</h2>
+
+        <button onClick={weeklyOnClick}>CONTINUAR</button>
 
 
 
